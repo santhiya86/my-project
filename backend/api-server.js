10f1bf0cdee12baa2311ebd50a01802645c9f653
santhiya86 <23csea27santhiya@gmail.com>
2026-04-05 14:33:01 +0530

    Fixed backend port issue

--- backend/api-server.js
+++ backend/api-server.js
@@ -114,7 +114,7 @@ app.get("/api/marks", async (req, res) => {
 });
 
 // Use Render's PORT
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7001;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
